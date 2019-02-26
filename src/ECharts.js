8d9a6e3c362b12d30cfa468d58f3b717b391533a
simonguo <simonguo.2009@gmail.com>
2019-02-26 12:38:17 +0800

    Fix: #3

--- src/ECharts.js
+++ src/ECharts.js
@@ -10,41 +10,33 @@ import { isSeriesOption } from './utils';
 
 const defaultOption = {
   grid: {
-    containLabel: true,
+    containLabel: true
   },
-  color: [
-    '#34c3ff',
-    '#a873e6',
-    '#7ed321',
-    '#13ba9e',
-    '#1464ac',
-    '#32a4d4',
-    '#f5a623',
-  ],
+  color: ['#34c3ff', '#a873e6', '#7ed321', '#13ba9e', '#1464ac', '#32a4d4', '#f5a623']
 };
-
 
 // ECharts with empty message and loading
 class ECharts extends Component {
-
   static propTypes = {
     height: PropTypes.number,
     color: PropTypes.arrayOf(PropTypes.string),
     locale: PropTypes.shape({
-      emptyMessage: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.node,
-      ]),
-      loading: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.node,
-      ]),
-    }),
+      emptyMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+      loading: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
+    })
+  };
+
+  static defaultProps = {
+    height: 300,
+    locale: {
+      emptyMessage: 'No data found',
+      loading: 'Loading...'
+    }
   };
 
   static childContextTypes = {
     setChartOption: PropTypes.func,
-    series: PropTypes.arrayOf(PropTypes.object),
+    series: PropTypes.arrayOf(PropTypes.object)
   };
 
   constructor(props) {
@@ -54,8 +46,8 @@ class ECharts extends Component {
     this.state = {
       option: {
         ...defaultOption,
-        color: color || defaultOption.color,
-      },
+        color: color || defaultOption.color
+      }
     };
   }
 
@@ -63,63 +55,54 @@ class ECharts extends Component {
     const { children } = this.props;
     return {
       setChartOption: this.setOption,
-      series: Children.toArray(children).filter(comp => isSeriesOption(comp)),
+      series: Children.toArray(children).filter(comp => isSeriesOption(comp))
     };
   }
 
   setOption = func => this.setState(({ option }) => ({ option: func(option) }));
 
-  render() {
-    const {
-      height = 300,
-      locale = {
-        emptyMessage: 'No data found',
-        loading: 'Loading...',
-      },
-      className,
-      style,
-      children,
-    } = this.props;
-    const { option } = this.state;
-    const dataEmpty = !option.series
-      || option.series.reduce(
-        (empty, serie) => empty && (!serie.data || serie.data.length < 1), true
-      );
+  bindEChartsRef = ref => {
+    this.echarts = ref && ref.getEchartsInstance();
+  };
 
-    function renderEmptyMessage() {
-      return (
-        <div
-          className="rs-echarts-body-info"
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {locale.emptyMessage}
-        </div>
-      );
-    }
+  renderEmptyMessage() {
+    const { locale } = this.props;
+    return (
+      <div
+        className="rs-echarts-body-info"
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        {locale.emptyMessage}
+      </div>
+    );
+  }
+
+  render() {
+    const { height, className, style, children } = this.props;
+    const { option } = this.state;
+    const dataEmpty =
+      !option.series ||
+      option.series.reduce((empty, serie) => empty && (!serie.data || serie.data.length < 1), true);
 
     return (
-      <div className={`rs-echarts ${className}`} style={{ height, ...style }}>
-        {
-          dataEmpty
-            ? renderEmptyMessage()
-            : (
-              <ReactEchartsCore
-                echarts={echarts}
-                option={option}
-                style={{ height: '100%' }}
-                notMerge
-                ref={(e) => {
-                  this.echarts = e && e.getEchartsInstance();
-                }}
-              />
-            )
-        }
+      <div className={`rs-echarts ${className || ''}`} style={{ height, ...style }}>
+        {dataEmpty ? (
+          this.renderEmptyMessage()
+        ) : (
+          <ReactEchartsCore
+            echarts={echarts}
+            option={option}
+            style={{ height: '100%' }}
+            ref={this.bindEChartsRef}
+            notMerge
+          />
+        )}
         {children}
       </div>
     );
