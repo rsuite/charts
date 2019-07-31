d74d9e229c8e99dc88bcac99e9556699d0212ead
Doma <leishenghao@126.com>
2019-07-31 15:10:42 +0800

    处理 vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,6 @@ const common = {
           {
             loader: 'markdown-loader',
             options: {
-              pedantic: true,
               renderer: markdownRenderer()
             }
           }
