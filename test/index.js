196a5881347fa062c57555bdc85c53ff21c4bcab
Doma <leishenghao@126.com>
2021-03-05 01:03:55 +0800

    chore: remove karma tests and deps

--- test/index.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
-const testsContext = require.context('.', true, /Spec$/);
-testsContext.keys().forEach(testsContext);
