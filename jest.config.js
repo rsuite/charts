657697eb3f5314db432525503b4c738388ef5b2c
Doma <leishenghao@126.com>
2023-08-01 14:54:33 +0800

    test: run test agains react 16,17,18

--- jest.config.js
+++ jest.config.js
@@ -16,3 +16,19 @@ module.exports = {
     '\\.svg': '<rootDir>/__mocks__/svg.js',
   },
 };
+
+if (process.env.REACT === '17') {
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    '^react((\\/.*)?)$': 'react-17$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-17$1',
+  };
+}
+
+if (process.env.REACT === '18') {
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    '^react((\\/.*)?)$': 'react-18$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-18$1',
+  };
+}
