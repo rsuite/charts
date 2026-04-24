372be8fe2b6720d71ed3e4b42c663995ded90a16
simonguo <simonguo.2009@gmail.com>
2026-04-24 11:46:49 +0800

    fix(test): add ResizeObserver and matchMedia polyfills for jsdom

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,23 @@
 import '@testing-library/jest-dom';
+
+// ResizeObserver is not implemented in jsdom (used by recharts ResponsiveContainer)
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+// matchMedia is not implemented in jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
